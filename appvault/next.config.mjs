e8ccc635f37c7ae3c98f141d27e72c6332a1d509
satriorafli19 <satriorafli1945@gmail.com>
2026-06-11 14:45:59 +0700

    fix: rename next.config.ts to next.config.mjs

--- appvault/next.config.mjs
+++ appvault/next.config.mjs
@@ -1,6 +1,4 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: 'export', // Ini kunci biar Vercel nggak error
   images: {
     unoptimized: true, // Biar nggak error masalah gambar
